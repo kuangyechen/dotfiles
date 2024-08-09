a45522fc0da83b6f9443e8a1e62852354ccafa21
Kuangye Chen <chen.kuangye@gmail.com>
2024-08-09 16:00:18 +0800

    update

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -11,7 +11,7 @@ set -Ua fish_user_paths "$HOME/.rye/shims"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    
+
     # Utility functions
     function command_exists
         command -v $argv[1] >/dev/null 2>&1
@@ -30,34 +30,51 @@ if status is-interactive
     # Eza
     if command_exists eza
         # general use aliases
-        alias ls='eza'                                  # just replace ls by eza and allow all other eza arguments
-        alias l='eza -lbF --git'                        # list, size, type, git
-        alias ll='eza -lbGF --git'                      # long, all
-        alias llm='eza -lbGd --git --sort=modified'     # list, long, sort by modification date
-        alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale'      # all list
-        alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'     # all list and extended
-        alias tree='eza --tree'                         # tree view
-        alias lS='eza -1'                               # one column by just names
-        alias lt='eza --tree --level=2'                 # tree level 2
+
+        # just replace ls by eza and allow all other eza arguments
+        abbr --add ls eza
+        # list, size, type, git
+        abbr --add l eza -lbF --git
+        # long, all
+        abbr --add ll eza -lbGF --git
+        # list, long, sort by modification date
+        abbr --add llm eza -lbGd --git --sort=modified
+        # all list
+        abbr --add la eza -lbhHigUmuSa --time-style=long-iso --git --color-scale
+        # all list and extended
+        abbr --add lx eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale
+        # tree view
+        abbr --add tree eza --tree
+        # one column by just names
+        abbr --add lS eza -1
+        # tree level 2
+        abbr --add lt eza --tree --level=2
     end
 
     # rm2trash
     if command_exists rm2trash
-        alias rm='rm2trash rm'
-        alias ls_trash='rm2trash ls'
-        alias cd_trash='cd $(rm2trash trash-path)'
-        alias empty_trash='rm2trash empty'
+        abbr --add rm rm2trash rm
+        abbr --add ls_trash rm2trash ls
+        abbr --add cd_trash cd $(rm2trash trash-path)
+        abbr --add empty_trash rm2trash empty
     end
 
     # Bat
     if command_exists bat
-        alias cat=bat
+        set -l CAT bat
+        abbr --add cat bat
+    else
+        set -l CAT cat
     end
+
+    # Alias
+    abbr --add cat_fish_config {$CAT} {$HOME}/.config/fish/config.fish
+    abbr --add cat_fish_variable {$CAT} {$HOME}/.config/fish/fish_variables
 
     # Environments
     set -g fish_key_bindings fish_vi_key_bindings
     set -g fish_greeting
-    set -g SSH_KEY_PATH "{$HOME}/.ssh/id_ed25519"
+    set -g SSH_KEY_PATH {$HOME}/.ssh/id_ed25519
 
     if command_exists hx
         set -g EDITOR hx
--- dots/fish/functions/cat_fish_variables.fish
+++
@@ -1,4 +0,0 @@
-function cat_fish_variables --wraps='cat {$HOME}/.config/fish/fish_variables' --wraps='cat ~/.config/fish/fish_variables' --description 'alias cat_fish_variables cat ~/.config/fish/fish_variables'
-  cat ~/.config/fish/fish_variables $argv
-        
-end
